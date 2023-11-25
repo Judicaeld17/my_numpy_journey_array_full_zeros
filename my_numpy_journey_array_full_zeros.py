37de5029795e7ab4e2b3b1d495c14afcae404a87
Judicaeld17e <judicaeldjidonou3@gmail.com>
2023-11-25 18:33:45 +0100

    first commit

---
+++ my_numpy_journey_array_full_zeros.py
@@ -0,0 +1,4 @@
+#TASK: Create a function which take dimension of an array parameter and returns a Numpy Array fill with 0. 
+import numpy as np
+def my_numpy_journey_array_full_zeros(array_dim):
+    return np.zeros(array_dim)
